c07181e98af6c1d1a9f93fbb1a70935c5d115013
Hugo Amoglia Priori <priori.hugo@estudante.ufjf.br>
2021-11-30 05:56:39 -0300

    adicionando quadradinho inimigo

--- script.js
+++ script.js
@@ -16,18 +16,18 @@ window.addEventListener('keyup' , teclaSolta , false);
 function teclaPressionada(e){
     e.preventDefault();
     //console.log(e.keyCode);
-    if(e.keyCode == 37) quadradoAX = -ACELERACAO;
-    if(e.keyCode == 38) quadradoAY = -ACELERACAO;
-    if(e.keyCode == 39) quadradoAX = ACELERACAO;
-    if(e.keyCode == 40) quadradoAY = ACELERACAO;
+    if(e.keyCode == 37) personagem.AX = -personagem.ACELERACAO;
+    if(e.keyCode == 38) personagem.AY = -personagem.ACELERACAO;
+    if(e.keyCode == 39) personagem.AX = personagem.ACELERACAO;
+    if(e.keyCode == 40) personagem.AY = personagem.ACELERACAO;
 }
 function teclaSolta(e){
     e.preventDefault();
     //console.log(e.keyCode);
-    if(e.keyCode == 37) quadradoAX = 0;
-    if(e.keyCode == 38) quadradoAY = 0;
-    if(e.keyCode == 39) quadradoAX = 0;
-    if(e.keyCode == 40) quadradoAY = 0;
+    if(e.keyCode == 37) personagem.AX = 0;
+    if(e.keyCode == 38) personagem.AY = 0;
+    if(e.keyCode == 39) personagem.AX = 0;
+    if(e.keyCode == 40) personagem.AY = 0;
 }
 
 //canvas config
@@ -50,6 +50,17 @@ let personagem = {
  VY: 0,
  AY: 0,
 };
+
+let inimigo = {
+    VELOCIDADE: 100,
+    ACELERACAO: 40,
+    X: (canvas.width/2) + 40,
+    VX: 0,
+    AX: 0,
+    Y: (canvas.height/2) + 100,
+    VY: 0,
+    AY: 0,
+   };
 let t0,dt,fps;
 
 requestAnimationFrame(loop);
@@ -65,18 +76,28 @@ function loop(t){
     if (personagem.Y > canvas.height + 20) personagem.Y = 0;
     if (personagem.Y < 0) personagem.Y = canvas.height;
 
+    inimigo.AY = (personagem.Y - inimigo.Y)
+    inimigo.AX = (personagem.X - inimigo.X)
+
+
     personagem.VX = personagem.VX + personagem.AX*dt;
     personagem.VY = personagem.VY + personagem.AY*dt;
     personagem.X = personagem.X + personagem.VX*dt;
     personagem.Y = personagem.Y + personagem.VY*dt;
 
+    inimigo.VX = inimigo.VX + inimigo.AX*dt;
+    inimigo.VY = inimigo.VY + inimigo.AY*dt;
+    inimigo.X = inimigo.X + inimigo.VX*dt;
+    inimigo.Y = inimigo.Y + inimigo.VY*dt;
     //fundo
     ctx.fillStyle = 'black'
     ctx.fillRect(0 , 0 , canvas.width , canvas.height)
 
     //quadradinho
+    ctx.fillStyle = 'red'
+    ctx.fillRect(inimigo.X,inimigo.Y, 20 , 20 )
+    requestAnimationFrame(loop);
     ctx.fillStyle = 'white'
     ctx.fillRect(personagem.X,personagem.Y, 20 , 20 )
-    requestAnimationFrame(loop);
     t0 = t;
 }
